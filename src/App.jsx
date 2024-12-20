5f6cca7bbc36a331f9e3cecca1f65b5ad9d34df0
axwvvvvv <galiyevamirbek84@gmail.com>
2024-12-20 16:47:52 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header/Header'
+
+import './App.css'
+import Hero from './components/Hero/Hero'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    </>
+  )
+}
+
+export default App
